08130e8f72684694a82fe1c3fae3add600a7a514
VladislavG <grishvm@yandex.ru>
2025-04-06 15:01:01 +0500

    верстка блока ошибки и успеха при отправке формы 1920-320, изменена структура разметки формы и модалки, отдельные scss-модули для кнопок, шаблона ошибки и успеха

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,17 +4,17 @@
 
 <template>
 		<div class="app-container">
-				<form class="form-feedback">
+				<div class="form-feedback">
 
-						<div class="form-feedback__head">
-								<button type="button" class="form-feedback__btn-close"></button>
-								<div class="form-feedback__title-form">
-										<h2>Форма обратной связи</h2>
-								</div>
-								<div class="form-feedback__subtitle-form">
-										<p>Пожалуйста, оцените свой опыт прохождения тестового</p>
-								</div>
+						<button type="button" class="form-feedback__btn-close"></button>
+						<div class="form-feedback__title-form">
+								<h2>Форма обратной связи</h2>
+						</div>
+						<div class="form-feedback__subtitle-form">
+								<p>Пожалуйста, оцените свой опыт прохождения тестового</p>
+						</div>
 
+						<form class="form-feedback__form">
 								<div class="form-feedback__rating">
 										<button type="button" class="form-feedback__star"></button>
 										<button type="button" class="form-feedback__star"></button>
@@ -22,6 +22,7 @@
 										<button type="button" class="form-feedback__star"></button>
 										<button type="button" class="form-feedback__star"></button>
 								</div>
+
 								<div class="form-feedback__benefits-wrap">
 										<button type="button" class="form-feedback__item-benefit active-item">Интересно</button>
 										<button type="button" class="form-feedback__item-benefit">Легко</button>
@@ -30,56 +31,76 @@
 										<button type="button" class="form-feedback__item-benefit">Подробно описано</button>
 										<button type="button" class="form-feedback__item-benefit">Все понятно и по делу</button>
 								</div>
-						</div>
 
-						<div class="form-feedback__body">
-								<div class="form-feedback__field-wrap form-feedback__field-wrap--name">
-										<label for="fullname">ФИО</label>
-										<input id="fullname" type="text" name="name" placeholder="Иван Иванов">
-								</div>
-								<div class="form-feedback__field-wrap form-feedback__field-wrap--email">
-										<label for="email">Почта</label>
-										<input id="email" type="email" placeholder="Текст">
-								</div>
-								<div class="form-feedback__field-wrap form-feedback__field-wrap--phone">
-										<label for="phone">Номер телефона</label>
-										<input id="phone" type="tel" placeholder="+7 (000) 000 00 00">
-								</div>
-								<div class="custom-select">
-										<span class="custom-select__label">Грейд</span>
-										<div class="custom-select__selected">Выберите</div>
-										<div class="custom-select__dropdown-wrap">
-												<ul class="custom-select__dropdown">
-														<li class="custom-select__option">Junior</li>
-														<li class="custom-select__option">Middle</li>
-														<li class="custom-select__option">Senior</li>
-														<li class="custom-select__option">Team Lead</li>
-														<li class="custom-select__option">Junior</li>
-														<li class="custom-select__option">Middle</li>
-														<li class="custom-select__option">Senior</li>
-														<li class="custom-select__option">Team Lead</li>
-												</ul>
+								<div class="form-feedback__fields-container">
+										<div class="form-feedback__field-wrap form-feedback__field-wrap--name">
+												<label for="fullname">ФИО</label>
+												<input id="fullname" type="text" name="name" placeholder="Иван Иванов">
+										</div>
+										<div class="form-feedback__field-wrap form-feedback__field-wrap--email">
+												<label for="email">Почта</label>
+												<input id="email" type="email" placeholder="Текст">
+										</div>
+										<div class="form-feedback__field-wrap form-feedback__field-wrap--phone">
+												<label for="phone">Номер телефона</label>
+												<input id="phone" type="tel" placeholder="+7 (000) 000 00 00">
+										</div>
+										<div class="custom-select">
+												<span class="custom-select__label">Грейд</span>
+												<div class="custom-select__selected">Выберите</div>
+												<div class="custom-select__dropdown-wrap">
+														<ul class="custom-select__dropdown">
+																<li class="custom-select__option">Junior</li>
+																<li class="custom-select__option">Middle</li>
+																<li class="custom-select__option">Senior</li>
+																<li class="custom-select__option">Team Lead</li>
+																<li class="custom-select__option">Junior</li>
+																<li class="custom-select__option">Middle</li>
+																<li class="custom-select__option">Senior</li>
+																<li class="custom-select__option">Team Lead</li>
+														</ul>
+												</div>
+										</div>
+										<div class="form-feedback__field-wrap form-feedback__field-wrap--message">
+												<label for="message">Дополнительная информация</label>
+												<textarea id="message" name="message" placeholder="Что понравилось и не понравилось"></textarea>
 										</div>
 								</div>
 
-								<div class="form-feedback__field-wrap form-feedback__field-wrap--message">
-										<label for="message">Дополнительная информация</label>
-										<textarea id="message" name="message" placeholder="Что понравилось и не понравилось"></textarea>
-								</div>
-						</div>
-
-						<div class="form-feedback__footer">
 								<div class="form-feedback__steps-wrap">
 										<span class="form-feedback__step active-step">1</span>
 										<span class="form-feedback__step-process">
-												<span class="form-feedback__step-process-line" style="width: 50%;"></span>
-										</span>
+													<span class="form-feedback__step-process-line" style="width: 50%;"></span>
+												</span>
 										<span class="form-feedback__step">2</span>
 								</div>
-								<button type="button" class="form-feedback__btn form-feedback__btn--white">Отменить</button>
-								<button type="submit" class="form-feedback__btn form-feedback__btn--blue">Отправить</button>
+
+								<div class="form-feedback__btns-wrap">
+								<button type="button" class="form-feedback__btn btn btn-white">Отменить</button>
+								<button type="submit" class="form-feedback__btn btn btn--blue">Отправить</button>
+
+
+								</div>
+						</form>
+
+
+						<div class="success">
+								<div class="success__icon icon-status">
+										<img src="../assets/images/success.svg" alt="Успешно отправлено">
+								</div>
+								<button type="button" class="success__revert-btn btn btn--blue">На главную</button>
 						</div>
-				</form>
+
+						<div class="error">
+								<div class="error__icon icon-status">
+										<img src="../assets/images/error.svg" alt="Ошибка">
+								</div>
+								<button type="button" class="error__cancel-btn btn btn--white">Закрыть</button>
+								<button type="button" class="error__retry-btn btn btn--blue">Попробовать снова</button>
+						</div>
+
+
+				</div>
 		</div>
 </template>
 
